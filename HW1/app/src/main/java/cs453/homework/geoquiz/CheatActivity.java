d1dba80d55c09221f48bcf94d55d8196fffec8fd
Zyc-K <yuchengmojian@gmail.com>
2020-09-06 06:40:33 -0700

    h2 start up

---
+++ HW1/app/src/main/java/cs453/homework/geoquiz/CheatActivity.java
@@ -0,0 +1,14 @@
+package cs453.homework.geoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
