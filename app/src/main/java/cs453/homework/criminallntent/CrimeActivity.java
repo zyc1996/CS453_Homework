74353e54d7df979c7e59ef4f28483f342398c140
Zyc-K <yuchengmojian@gmail.com>
2020-09-12 16:13:13 -0700

    setup

---
+++ app/src/main/java/cs453/homework/criminallntent/CrimeActivity.java
@@ -0,0 +1,14 @@
+package cs453.homework.criminallntent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
