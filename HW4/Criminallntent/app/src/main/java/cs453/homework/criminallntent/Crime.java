c67d2452189fd0f1d7e41e2300074cfb58ad3c66
Zyc-K <yuchengmojian@gmail.com>
2020-09-25 00:43:50 -0700

    prep hw4

---
+++ HW4/Criminallntent/app/src/main/java/cs453/homework/criminallntent/Crime.java
@@ -0,0 +1,48 @@
+package cs453.homework.criminallntent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId; //crime id
+    private String mTitle; //crime title
+    private Date mDate; //crime date
+    private boolean mSolved; //is crime solved?
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public void setmId(UUID mId) {
+        this.mId = mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean ismSolved() {
+        return mSolved;
+    }
+
+    public void setmSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+}
