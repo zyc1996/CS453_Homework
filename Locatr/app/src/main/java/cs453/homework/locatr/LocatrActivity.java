eb4f7972a750aef2977b59f568fde7f7a8add932
Zyc-K <yuchengmojian@gmail.com>
2020-12-02 13:06:13 -0800

    finalize hw9

---
+++ Locatr/app/src/main/java/cs453/homework/locatr/LocatrActivity.java
@@ -0,0 +1,14 @@
+package cs453.homework.locatr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocatrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locatr);
+    }
+}
